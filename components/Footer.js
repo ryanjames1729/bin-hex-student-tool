f534184f6a74c12df64d8b755330c43015a454fb
rjames <rjames@rjames-mbpr-us.lan>
2022-06-10 19:34:04 -0400

    binary tool grid setup

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { SocialIcon } from 'react-social-icons';
+import Link from 'next/link';
+
+const Footer = ({landingPages}) => {
+    return (
+        <footer className="bg-darkBlue" id="footer">
+            <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+                <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+                    <div className="mx-auto my-6 text-center text-lightGray md:hidden">
+                        Copyright &copy; 2022, All Rights Reserved
+                    </div>
+                    <div>
+                        <h1 className="text-xl text-lightGray">This tool was made by Ryan James</h1>
+                    </div>
+                    <div className="flex justify-center space-x-4">
+                        <Link href="https://twitter.com/rjames1729"><SocialIcon url="https://twitter.com/rjames1729" network="twitter" bgColor="#2B2D42" fgColor="#EDF2F4" /></Link>
+                        <Link href="https://www.linkedin.com/in/ryan-james-562612172"><SocialIcon url="https://www.linkedin.com/in/ryan-james-562612172" network="linkedin" bgColor="#2B2D42" fgColor="#EDF2F4" /></Link>
+                        <Link href="https://github.com/ryanjames1729"><SocialIcon url="https://github.com/ryanjames1729" network="github" bgColor="#2B2D42" fgColor="#EDF2F4" /></Link>
+                        <Link href="mailto:ryanjames1729@gmail.com"><SocialIcon url="mailto:ryanjames1729@gmail.com" network="email" bgColor="#2B2D42" fgColor="#EDF2F4" /></Link>
+                    </div>
+                </div>
+
+                <div className="flex justify-around space-x-32">
+                    <div className="flex flex-col space-y-3 text-lightGray">
+                        <Link href='https://ryanjameswritescode.com'>Blog</Link>
+                        <Link href="https://github.com/ryanjames1729">Github</Link>
+                        <Link href="#footer">Contact</Link>
+                    </div>
+                    <div className="flex flex-col space-y-3 text-lightGray">
+                        <Link href="/">Ryan James</Link>
+                        <Link href="mailto:ryanjames1729@gmail.com">ryanjames1729@gmail.com</Link>
+                        <Link href="#">You can find me in the 828</Link>
+                    </div>
+                
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
+
---
+++ components/Navbar.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState } from "react";
+import Link from 'next/link';
+import Script from 'next/script'
+
+import { GraphQLClient } from 'graphql-request'
+
+
+const Navbar = ({ landingPages }) => {
+  
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  return (
+    <div className="w-screen shadow-2xl h-40 lg:h-24">
+    <div className="flex items-center justify-between px-8 py-8">
+      <Link href="/" className="text-xl">
+        bin-hex-student-tool
+      </Link>
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2 px-8 hamburger block"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="absolute top-0 right-0 px-4 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+                <div className="space-y-2 px-2 hamburger open block">
+                    <span className="animate-pulse hamburger-top"></span>
+                    <span className="animate-pulse hamburger-middle"></span>
+                    <span className="animate-pulse hamburger-bottom"></span>
+                </div>
+                <ul className="flex flex-col items-right justify-between min-h-[100px]">
+                    <li className="border-b border-gray-400 my-2 uppercase">
+                        <Link href="#">BINARY</Link>
+                    </li>
+                    <li className="border-b border-gray-400 my-2 uppercase">
+                        <Link href="#">HEXADECIMAL</Link>
+                     </li>
+                    <li className="border-b border-gray-400 my-2 uppercase">
+                        <Link href="#footer">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+          </div>
+        </section>
+
+        <ul className="DESKTOP-MENU hidden uppercase space-x-8 lg:flex">
+          <li>
+            <Link href="#">BINARY</Link>
+          </li>
+          <li>
+            <Link href="#">HEXADECIMAL</Link>
+          </li>
+          <li>
+            <Link href="#footer">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    </div>
+  );
+}
+
+
+
+export default Navbar
